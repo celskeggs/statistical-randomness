a607b1f1d5ef24f3ef2508b6c24c5ec015953eeb
Cel Skeggs <git@celskeggs.com>
2017-02-13 22:37:15 -0500

    Add basic curses example

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("STATS STATS STATS STATS")
+import curses
+
+stdscr = curses.initscr()
+try:
+    stdscr.addstr(5, 10, "STATS " * 4)
+    stdscr.refresh()
+    stdscr.getkey()
+finally:
+    curses.endwin()
